c968203daf213cafe04a70e11819957a90d83112
mattimoller <mathias.moller1@gmail.com>
2020-11-22 21:58:09 +0100

    Added counties to map, removed cities

--- apps/page_1.py
+++ apps/page_1.py
@@ -13,7 +13,9 @@ import pandas as pd
 import plotly.express as px
 import sys
 from app import app
-
+import random
+import json
+from urllib.request import urlopen
 # external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 colors = {
@@ -31,13 +33,39 @@ df_norway = df_norway.rename(columns={'population': 'Inbyggertall'})
 df_norway['Antall ansatte'] = (df_norway['Inbyggertall']*0.001).astype(int)
 df_norway['Sykefravær siste mnd (%)'] = np.random.randint(2, 20, size=(len(df_norway.index), 1))
 
+fylker = ['Rogaland', 'Finnmark', 'Troms', 'Møre og Romsdal', 'Hordaland', 'Telemark', 'Vestfold', 'Østfold', 'Buskerud', 'Oslo', 'Akershus', 'Oppland', 'Hedmark', 'Nordland', 'Aust-Agder', 'Vest-Agder', 'Trøndelag', 'Sogn og Fjordane']
+idList = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15", "16", "17", "18"]
 
-def generate_store_map(df_norway):
+sykfra = np.random.randint(
+    low=2, high=20, size=(len(fylker)), dtype='int')
+# all_trans = np.hstack((income, expenses * -1))
+df_counties = pd.DataFrame(
+    {'Fylke': fylker, 'Sykefravær': sykfra, 'id': idList})
+with open('assets/gamle_fylker.json') as json_file:
+    counties = json.load(json_file)
+
+def generate_cities_map(df_norway):
     map_fig = px.scatter_mapbox(df_norway, lat="lat", lon="lng", hover_name="city", hover_data=["Antall ansatte", "Sykefravær siste mnd (%)"],
                         color_discrete_sequence=["#006458"], zoom=3, height=800, width=400)
     map_fig.update_layout(mapbox_style="open-street-map")
     map_fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
     return map_fig
+
+def generate_counties_map(df_counties, geojson_file):
+    fig = px.choropleth_mapbox(df_counties, geojson=geojson_file ,locations='id', color='Sykefravær',
+                            color_continuous_scale="Viridis",
+                            labels={'Fylke':'Fylke'},
+                            mapbox_style='open-street-map',
+                            center={"lat": 65.4305, "lon": 13.3951},
+                            zoom=3,
+                            height=600,
+                            width=00,
+                          )
+    fig.update_geos(fitbounds="locations", visible=False, resolution=50, projection_type="conic equidistant")
+    fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+    fig.update_traces(showlegend=False, marker_opacity=0.5)
+    fig.update(layout_coloraxis_showscale=False)
+    return fig
 
 # If running in a single.page app use app.layout = html.Div()....., if running in multi-page app use layout = html.Div()
 layout=html.Div([
@@ -45,22 +73,23 @@ layout=html.Div([
         dbc.Row(
             dbc.Col(
                 html.H2(
-                    "Butikkoversikt",
+                    "Distriktsoversikt",
                     className="text-center text-dark",
-                )
+                ),
                 #width={"size": 6, "offset": 3}
+                style={'margin-top': '25px'}
             ),  
         ),
         
                 # Insert international transactions map and company structure
         dbc.Row([
-            dbc.Col(dcc.Graph(figure=generate_store_map(df_norway)),width=4),
+            dbc.Col(dcc.Graph(figure=generate_counties_map(df_counties, counties)),width=4),
             dbc.Col([
                 dbc.Row(
                     dbc.Col(
                             dbc.FormGroup(
                                 [
-                                    dbc.Label("Velg butikk", className="mr-2"),
+                                    dbc.Label("Velg distrikt", className="mr-2"),
                                     dcc.Dropdown(
                                         options=[
                                             {'label': i, 'value': i} for i in df_norway.city
